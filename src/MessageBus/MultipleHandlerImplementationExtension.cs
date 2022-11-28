926daa4d0a65cc674e377f64668406052d4653ec
tomatosalat0 <david.huettig@gmx.net>
2022-11-28 12:56:30 +0100

    Added extension which allows to register a message handler which implements multiple handle interfaces for different message types

---
+++ src/MessageBus/MultipleHandlerImplementationExtension.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using MessageBus.Messaging;
+
+namespace MessageBus
+{
+    public static class MultipleHandlerImplementationExtension
+    {
+        /// <summary>
+        /// Searches for all <see cref="IMessageEventHandler{TEvent}"/> or <see cref="IAsyncMessageEventHandler{TEvent}"/>
+        /// implementations within the instance of <paramref name="handler"/> and registers each implementation
+        /// to the provided <paramref name="messageBus"/>. The method returns a list of all subscription disposables
+        /// returned by <see cref="IMessageBusHandler.RegisterEventHandler{TEvent}(IMessageEventHandler{TEvent})"/> or
+        /// <see cref="IMessageBusHandler.RegisterEventHandler{TEvent}(IAsyncMessageEventHandler{TEvent})"/>.
+        /// </summary>
+        /// <remarks>If an exception during the regiration process happens, all previously generated subscriptions which happened within
+        /// this method will get rolled back.</remarks>
+        /// <exception cref="ArgumentException">Is thrown if the registration methods could not be found within <paramref name="messageBus"/>.</exception>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="handler"/> is null.</exception>
+        /// <exception cref="InvalidOperationException">Is thrown if <paramref name="handler"/> does not implement any <see cref="IMessageEventHandler{TEvent}"/>
+        /// or <see cref="IAsyncMessageEventHandler{TEvent}"/>.</exception>
+        /// <exception cref="TargetInvocationException">Is thrown if the registration failed.</exception>
+        public static IReadOnlyList<IDisposable> RegisterAllEventHandlers(this IMessageBusHandler messageBus, object handler)
+        {
+            if (handler is null) throw new ArgumentNullException(nameof(handler));
+
+            return RegisterAllHandlers(
+                messageBus,
+                handler,
+                registerMethodName: nameof(IMessageBusHandler.RegisterEventHandler),
+                explicitInterfaceMethodName: $"{typeof(IMessageBusHandler).FullName}.{nameof(IMessageBusHandler.RegisterEventHandler)}",
+                interfaceTypes: new[]
+                {
+                    typeof(IMessageEventHandler<>),
+                    typeof(IAsyncMessageEventHandler<>)
+                });
+        }
+
+        /// <summary>
+        /// Searches for all <see cref="IMessageCommandHandler{TEvent}"/> or <see cref="IAsyncMessageCommandHandler{TEvent}"/>
+        /// implementations within the instance of <paramref name="handler"/> and registers each implementation
+        /// to the provided <paramref name="messageBus"/>. The method returns a list of all subscription disposables
+        /// returned by <see cref="IMessageBusHandler.RegisterCommandHandler{TEvent}(IMessageCommandHandler{TEvent})"/> or
+        /// <see cref="IMessageBusHandler.RegisterCommandHandler{TEvent}(IAsyncMessageCommandHandler{TEvent})"/>.
+        /// </summary>
+        /// <remarks>If an exception during the regiration process happens, all previously generated subscriptions which happened within
+        /// this method will get rolled back.</remarks>
+        /// <exception cref="ArgumentException">Is thrown if the registration methods could not be found within <paramref name="messageBus"/>.</exception>
+        /// <exception cref="ArgumentNullException">Is thrown if <paramref name="handler"/> is null.</exception>
+        /// <exception cref="InvalidOperationException">Is thrown if <paramref name="handler"/> does not implement any <see cref="IMessageCommandHandler{TEvent}"/>
+        /// or <see cref="IAsyncMessageCommandHandler{TEvent}"/>.</exception>
+        /// <exception cref="TargetInvocationException">Is thrown if the registration failed.</exception>
+        public static IReadOnlyList<IDisposable> RegisterAllCommandHandlers(this IMessageBusHandler messageBus, object handler)
+        {
+            if (handler is null) throw new ArgumentNullException(nameof(handler));
+
+            return RegisterAllHandlers(
+                messageBus,
+                handler,
+                registerMethodName: nameof(IMessageBusHandler.RegisterCommandHandler),
+                explicitInterfaceMethodName: $"{typeof(IMessageBusHandler).FullName}.{nameof(IMessageBusHandler.RegisterCommandHandler)}",
+                interfaceTypes: new[]
+                {
+                    typeof(IMessageCommandHandler<>),
+                    typeof(IAsyncMessageCommandHandler<>)
+                });
+        }
+
+        private static IReadOnlyList<IDisposable> RegisterAllHandlers(
+            IMessageBusHandler messageBus,
+            object handler,
+            string registerMethodName,
+            string explicitInterfaceMethodName,
+            IReadOnlyList<Type> interfaceTypes)
+        {
+            List<IDisposable> result = new List<IDisposable>();
+            try
+            {
+                foreach (Type interfaceType in interfaceTypes)
+                {
+                    MethodInfo registerEventHandler = GetRegistrationMethod(messageBus, registerMethodName, explicitInterfaceMethodName, interfaceType);
+                    result.AddRange(
+                        CollectInterfaceGenericTypes(handler, interfaceType)
+                            .Select(eventType => AssignHandler(messageBus, handler, registerEventHandler, eventType))
+                    );
+                }
+            }
+            catch (Exception)
+            {
+                // rollback
+                foreach (var p in result)
+                    p.Dispose();
+
+                throw;
+            }
+
+            if (result.Count == 0)
+                throw new InvalidOperationException($"No event handler implementation found in '{handler.GetType().FullName}'");
+
+            return result;
+        }
+
+        /// <summary>
+        /// Calls the event registration method of <paramref name="messageBus"/> within the instance provided by <paramref name="handler"/>.
+        /// The method to call is defined by <paramref name="invoke"/>. Because the registration methods have a generic type parameter,
+        /// the provided <paramref name="eventType"/> defines the actual type to pass.
+        /// </summary>
+        private static IDisposable AssignHandler(IMessageBusHandler messageBus, object handler, MethodInfo invoke, Type eventType)
+        {
+            object? subscription = invoke.MakeGenericMethod(eventType).Invoke(messageBus, new[] { handler });
+            if (subscription is not IDisposable disposableSubscription)
+                throw new InvalidOperationException($"Registration did not return a disposable");
+            return disposableSubscription;
+        }
+
+        /// <summary>
+        /// Return the generic type of all interfaces of the given <paramref name="interfaceType"/> which
+        /// are implemented by the provided <paramref name="handler"/>.
+        /// </summary>
+        private static IEnumerable<Type> CollectInterfaceGenericTypes(object handler, Type interfaceType)
+        {
+            return handler
+                .GetType()
+                .GetInterfaces()
+                .Where(p => p.IsGenericType)
+                .Where(p => p.GetGenericTypeDefinition() == interfaceType)
+                .Select(p => p.GetGenericArguments().FirstOrDefault())
+                .WhereNotNull();
+        }
+
+        private static MethodInfo GetRegistrationMethod(IMessageBusHandler messageBus, string methodName, string explicitMethodName, Type eventHandlerInterfaceType)
+        {
+            MethodInfo[] potentialMethods = messageBus.GetType()
+                .GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(p => p.MemberType == MemberTypes.Method && !p.IsStatic && p.ContainsGenericParameters)
+                .Where(p => p.ReturnType == typeof(IDisposable))
+                .Where(p =>
+                {
+                    ParameterInfo[] arguments = p.GetParameters();
+                    return arguments.Length == 1 &&
+                        arguments[0].ParameterType.IsGenericType &&
+                        arguments[0].ParameterType.GetGenericTypeDefinition() == eventHandlerInterfaceType;
+                })
+                .ToArray();
+
+            MethodInfo[] implicitInterfaceImplementation = potentialMethods
+                .Where(p => p.Name == methodName)
+                .ToArray();
+
+            MethodInfo[] explicitInterfaceImplementation = potentialMethods
+                .Where(p => p.Name == explicitMethodName)
+                .ToArray();
+
+            if (explicitInterfaceImplementation.Length == 1)
+                return explicitInterfaceImplementation[0];
+
+            if (potentialMethods.Length == 0)
+                throw new ArgumentException($"Could not find the needed '{methodName}' method");
+
+            if (potentialMethods.Length > 1)
+                throw new ArgumentException($"Found {potentialMethods.Length} methods, but expected exactly one matching method for '{methodName}'");
+
+            return potentialMethods[0];
+        }
+    }
+}
---
+++ tests/MessageBus.Tests/UnitTests/MultipleEventTypesTests.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MessageBus.Tests.UnitTests
+{
+    [TestClass]
+    public class MultipleEventTypesTests
+    {
+        [TestMethod]
+        public void RegisterAllThrowsExceptionOnNull()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            Assert.ThrowsException<ArgumentNullException>(() => bus.RegisterAllEventHandlers(null));
+        }
+
+        [TestMethod]
+        public void RegisterAllThrowsExceptionIfObjectDoesNotImplementAnyHandler()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            Assert.ThrowsException<InvalidOperationException>(() => bus.RegisterAllEventHandlers(new object()));
+        }
+
+        [TestMethod]
+        public void RegisterSucceedsIfMessageBusIsExplicitelyImplemented()
+        {
+            IMessageBusHandler bus = new ExplicitMessageHandlerImplementation();
+
+            IReadOnlyList<IDisposable> subscriptions = bus.RegisterAllEventHandlers(new MyMultiEventHandler());
+            Assert.AreEqual(3, subscriptions.Count);
+        }
+
+        [TestMethod]
+        public void RegisterSucceedsIfMessageBusIsImplicitelyImplemented()
+        {
+            IMessageBusHandler bus = new ImplicitMessageHandlerImplementation();
+
+            IReadOnlyList<IDisposable> subscriptions = bus.RegisterAllEventHandlers(new MyMultiEventHandler());
+            Assert.AreEqual(3, subscriptions.Count);
+        }
+
+        [TestMethod]
+        public void RegisterThrowsTargetInvokationExceptionIfMessageBusFails()
+        {
+            IMessageBusHandler bus = new AlwaysFailMessageHandler();
+
+            Assert.ThrowsException<TargetInvocationException>(() => bus.RegisterAllEventHandlers(new MyMultiEventHandler()));
+        }
+
+        [TestMethod]
+        public async Task HandlerWithMultipleTypesCanGetAdded()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            MyMultiEventHandler handler = new MyMultiEventHandler();
+            bus.RegisterAllEventHandlers(handler);
+
+            await bus.FireEvent(new MyEventA());
+            await Task.Delay(100);
+
+            Assert.AreEqual(1, handler.MyEvent_A_Calls);
+            Assert.AreEqual(0, handler.MyEvent_B_Calls);
+            Assert.AreEqual(0, handler.MyEvent_C_Calls);
+
+            await bus.FireEvent(new MyEventB());
+            await Task.Delay(100);
+
+            Assert.AreEqual(1, handler.MyEvent_A_Calls);
+            Assert.AreEqual(1, handler.MyEvent_B_Calls);
+            Assert.AreEqual(0, handler.MyEvent_C_Calls);
+
+            await bus.FireEvent(new MyEventC());
+            await Task.Delay(100);
+
+            Assert.AreEqual(1, handler.MyEvent_A_Calls);
+            Assert.AreEqual(1, handler.MyEvent_B_Calls);
+            Assert.AreEqual(1, handler.MyEvent_C_Calls);
+        }
+
+        [TestMethod]
+        public async Task MultipleCallsOfRegisterAllWillRegisterEverythingAgain()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            MyMultiEventHandler handler = new MyMultiEventHandler();
+            bus.RegisterAllEventHandlers(handler);
+            bus.RegisterAllEventHandlers(handler);
+
+            await bus.FireEvent(new MyEventA());
+            await bus.FireEvent(new MyEventB());
+            await bus.FireEvent(new MyEventC());
+            await Task.Delay(100);
+
+            Assert.AreEqual(2, handler.MyEvent_A_Calls);
+            Assert.AreEqual(2, handler.MyEvent_B_Calls);
+            Assert.AreEqual(2, handler.MyEvent_C_Calls);
+        }
+
+        [TestMethod]
+        public void RegisterEventHandlerReturnsAllSubscriptionDisposables()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            MyMultiEventHandler handler = new MyMultiEventHandler();
+            IReadOnlyList<IDisposable> subscriptions = bus.RegisterAllEventHandlers(handler);
+            Assert.AreEqual(3, subscriptions.Count);
+            Assert.AreEqual(3, subscriptions.Distinct().Count());
+        }
+
+        [TestMethod]
+        public async Task ReturnedSubscriptionsDisposablesBelongToEventHandler()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            MyMultiEventHandler handler = new MyMultiEventHandler();
+            IReadOnlyList<IDisposable> subscriptions = bus.RegisterAllEventHandlers(handler);
+
+            foreach (var p in subscriptions)
+                p.Dispose();
+
+            await bus.FireEvent(new MyEventA());
+            await bus.FireEvent(new MyEventB());
+            await bus.FireEvent(new MyEventC());
+            await Task.Delay(100);
+
+            Assert.AreEqual(0, handler.MyEvent_A_Calls);
+            Assert.AreEqual(0, handler.MyEvent_B_Calls);
+            Assert.AreEqual(0, handler.MyEvent_C_Calls);
+        }
+
+        [TestMethod]
+        public async Task RegistrationExceptionWillRollbackPreviouslyRegisteredHandlers()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            MyMultiHandlerWithAnIncompleteEvent handler = new MyMultiHandlerWithAnIncompleteEvent();
+            Assert.ThrowsException<TargetInvocationException>(() => bus.RegisterAllEventHandlers(handler));
+
+            await bus.FireEvent(new MyEventA());
+            await Task.Delay(100);
+
+            Assert.AreEqual(0, handler.MyEvent_A_Calls);
+            Assert.AreEqual(0, handler.MyEvent_Incomplete_Calls);
+        }
+
+        [TestMethod]
+        public async Task RegisterationExceptionWillOnlyRollbackWhatHappenedWithinThisMethod()
+        {
+            using IMessageBus bus = new MessageBrokerMessageBus(MemoryMessageBrokerBuilder.InProcessBroker(), NoExceptionNotification.Instance);
+
+            MyMultiHandlerWithAnIncompleteEvent handler = new MyMultiHandlerWithAnIncompleteEvent();
+            // this registration is still valid.
+            bus.RegisterEventHandler<MyEventA>(handler);
+
+            Assert.ThrowsException<TargetInvocationException>(() => bus.RegisterAllEventHandlers(handler));
+
+            await bus.FireEvent(new MyEventA());
+            await Task.Delay(100);
+
+            // will be "1" because of the previous manual registration
+            Assert.AreEqual(1, handler.MyEvent_A_Calls);
+            Assert.AreEqual(0, handler.MyEvent_Incomplete_Calls);
+        }
+
+        [Topic("Events/EventA")]
+        public class MyEventA : IMessageEvent
+        {
+            public MessageId MessageId { get; } = MessageId.NewId();
+        }
+
+        [Topic("Events/EventB")]
+        public class MyEventB : IMessageEvent
+        {
+            public MessageId MessageId { get; } = MessageId.NewId();
+        }
+
+        [Topic("Events/EventC")]
+        public class MyEventC : IMessageEvent
+        {
+            public MessageId MessageId { get; } = MessageId.NewId();
+        }
+
+        // the missing topic attribute is intential
+        public class MyIncompleteConfiguredEvent : IMessageEvent
+        {
+            public MessageId MessageId { get; } = MessageId.NewId();
+        }
+
+        private class MyMultiEventHandler :
+            IMessageEventHandler<MyEventA>,
+            IMessageEventHandler<MyEventB>,
+            IAsyncMessageEventHandler<MyEventC>
+        {
+            public int MyEvent_A_Calls { get; private set; }
+            public int MyEvent_B_Calls { get; private set; }
+            public int MyEvent_C_Calls { get; private set; }
+
+            public void Handle(MyEventA @event)
+            {
+                MyEvent_A_Calls++;
+            }
+
+            public void Handle(MyEventB @event)
+            {
+                MyEvent_B_Calls++;
+            }
+
+            public Task HandleAsync(MyEventC @event)
+            {
+                MyEvent_C_Calls++;
+                return Task.CompletedTask;
+            }
+        }
+
+        private class MyMultiHandlerWithAnIncompleteEvent :
+            IMessageEventHandler<MyEventA>,
+            IMessageEventHandler<MyIncompleteConfiguredEvent>
+        {
+            public int MyEvent_A_Calls { get; private set; }
+            public int MyEvent_Incomplete_Calls { get; private set; }
+
+            public void Handle(MyEventA @event)
+            {
+                MyEvent_A_Calls++;
+            }
+
+            public void Handle(MyIncompleteConfiguredEvent @event)
+            {
+                MyEvent_Incomplete_Calls++;
+            }
+
+        }
+
+        private class NoopDispose : IDisposable
+        {
+            public void Dispose()
+            {
+            }
+        }
+
+        private class ExplicitMessageHandlerImplementation : IMessageBusHandler
+        {
+            IDisposable IMessageBusHandler.RegisterCommandHandler<TCommand>(IMessageCommandHandler<TCommand> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterCommandHandler<TCommand>(IAsyncMessageCommandHandler<TCommand> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterEventHandler<TEvent>(IMessageEventHandler<TEvent> handler)
+            {
+                return new NoopDispose();
+            }
+
+            IDisposable IMessageBusHandler.RegisterEventHandler<TEvent>(IAsyncMessageEventHandler<TEvent> handler)
+            {
+                return new NoopDispose();
+            }
+
+            IDisposable IMessageBusHandler.RegisterQueryHandler<TQuery, TQueryResult>(IMessageQueryHandler<TQuery, TQueryResult> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterQueryHandler<TQuery, TQueryResult>(IAsyncMessageQueryHandler<TQuery, TQueryResult> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterRpcHandler<TRpc, TRpcResult>(IMessageRpcHandler<TRpc, TRpcResult> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterRpcHandler<TRpc, TRpcResult>(IAsyncMessageRpcHandler<TRpc, TRpcResult> handler) => throw new NotImplementedException();
+        }
+
+        private class ImplicitMessageHandlerImplementation : IMessageBusHandler
+        {
+            public IDisposable RegisterCommandHandler<TCommand>(IMessageCommandHandler<TCommand> handler) where TCommand : IMessageCommand => throw new NotImplementedException();
+
+            public IDisposable RegisterCommandHandler<TCommand>(IAsyncMessageCommandHandler<TCommand> handler) where TCommand : IMessageCommand => throw new NotImplementedException();
+
+            public IDisposable RegisterEventHandler<TEvent>(IMessageEventHandler<TEvent> handler) where TEvent : IMessageEvent
+            {
+                return new NoopDispose();
+            }
+
+            public IDisposable RegisterEventHandler<TEvent>(IAsyncMessageEventHandler<TEvent> handler) where TEvent : IMessageEvent
+            {
+                return new NoopDispose();
+            }
+
+            public IDisposable RegisterQueryHandler<TQuery, TQueryResult>(IMessageQueryHandler<TQuery, TQueryResult> handler)
+                where TQuery : IMessageQuery<TQueryResult>
+                where TQueryResult : IMessageQueryResult => throw new NotImplementedException();
+
+            public IDisposable RegisterQueryHandler<TQuery, TQueryResult>(IAsyncMessageQueryHandler<TQuery, TQueryResult> handler)
+                where TQuery : IMessageQuery<TQueryResult>
+                where TQueryResult : IMessageQueryResult => throw new NotImplementedException();
+
+            public IDisposable RegisterRpcHandler<TRpc, TRpcResult>(IMessageRpcHandler<TRpc, TRpcResult> handler)
+                where TRpc : IMessageRpc<TRpcResult>
+                where TRpcResult : IMessageRpcResult => throw new NotImplementedException();
+
+            public IDisposable RegisterRpcHandler<TRpc, TRpcResult>(IAsyncMessageRpcHandler<TRpc, TRpcResult> handler)
+                where TRpc : IMessageRpc<TRpcResult>
+                where TRpcResult : IMessageRpcResult => throw new NotImplementedException();
+        }
+
+        private class AlwaysFailMessageHandler : IMessageBusHandler
+        {
+            IDisposable IMessageBusHandler.RegisterCommandHandler<TCommand>(IMessageCommandHandler<TCommand> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterCommandHandler<TCommand>(IAsyncMessageCommandHandler<TCommand> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterEventHandler<TEvent>(IMessageEventHandler<TEvent> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterEventHandler<TEvent>(IAsyncMessageEventHandler<TEvent> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterQueryHandler<TQuery, TQueryResult>(IMessageQueryHandler<TQuery, TQueryResult> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterQueryHandler<TQuery, TQueryResult>(IAsyncMessageQueryHandler<TQuery, TQueryResult> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterRpcHandler<TRpc, TRpcResult>(IMessageRpcHandler<TRpc, TRpcResult> handler) => throw new NotImplementedException();
+
+            IDisposable IMessageBusHandler.RegisterRpcHandler<TRpc, TRpcResult>(IAsyncMessageRpcHandler<TRpc, TRpcResult> handler) => throw new NotImplementedException();
+        }
+    }
+}
